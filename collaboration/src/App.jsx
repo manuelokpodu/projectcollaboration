de409a2702f9035a087895d5f8e2641827e80112
Okpodu Emmanuel <manuelokpodu@gmail.com>
2025-01-09 14:13:03 +0100

    first commit

---
+++ collaboration/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>This is Team B</h1>
+      <p>We are working on a project called "TOETALLY"</p>
+
+      <div>
+        <p>We are definitely going to succeed</p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure ut
+        possimus voluptatum assumenda voluptas accusamus architecto numquam aut
+        similique facilis, eius alias porro sequi at blanditiis laudantium. Ea,
+        ratione ducimus!
+      </div>
+    </>
+  );
+}
+
+export default App;
